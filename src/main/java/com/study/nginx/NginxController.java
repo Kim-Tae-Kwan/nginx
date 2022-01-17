12e889da374b33d98dbd2b988eea148178fc2d6c
KTK <vodjaos@gmail.com>
2022-01-17 22:59:39 +0900

    [ADD] reverse proxy 테스트 controller 추가.

--- src/main/java/com/study/nginx/NginxController.java
+++ src/main/java/com/study/nginx/NginxController.java
@@ -1,14 +1,21 @@
 package com.study.nginx;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class NginxController {
-    @GetMapping
+    @Value("${test.name}")
+    private String name;
+
+    @GetMapping("/" + "${test.name}")
     public ResponseEntity<Object> hello(){
-        return new ResponseEntity<>("hello", HttpStatus.OK);
+
+        return new ResponseEntity<>(name + " hello", HttpStatus.OK);
     }
 }
